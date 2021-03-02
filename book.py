b82596578dca6b072f92412afdc8659ca93b9c19
Clément DAVID <clement.david@edu.devinci.fr>
2021-03-02 18:31:16 +0100

    Fill the main file

---
+++ book.py
@@ -0,0 +1,6 @@
+class Order:
+
+    def __init__(self, quantity, price, buy=True):
+        self.quantity = quantity
+        self.price = price
+        self.buy = buy
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from book import Book
+# from book import Book
 
 def main():
     book = Book("TEST")
@@ -10,6 +10,7 @@ def main():
     book.insert_buy(2, 11.0)
     book.insert_sell(1, 10.0)
     book.insert_sell(10, 10.0)
+
 
 if __name__ == "__main__":
     main()
