7dd9f4b598dd3d1c6d130b9cad5859ea5261a0f1
Clément DAVID <clement.david@edu.devinci.fr>
2021-03-05 21:00:47 +0100

    Implementation Order Class

--- order.py
+++ order.py
@@ -1,7 +1,9 @@
 class Order:
-    def __init__(self, quantity, price):
+
+    def __init__(self, quantity, price, _type):
         self.quantity = quantity
         self.price = price
+        self.type = type
 
-    def insert_buy(self):
-        pass
+    def __str__(self):  # human-readable content
+        return self._type + "%s @ %s" % (self.quantity, self.price)
