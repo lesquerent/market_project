06be9e41f77286920ce97b506d4f30c5de56a941
lesquerent <thibaultls@outlook.fr>
2021-03-06 01:22:31 +0100

    Added a static variable which correspond to the id of the order and modify __str__() method in consequences Added some method to sort orders Added some getters

--- order.py
+++ order.py
@@ -1,9 +1,39 @@
-class Order:
+import itertools
 
-    def __init__(self, quantity, price, _type):
-        self.quantity = quantity
-        self.price = price
-        self.type = type
+from functools import total_ordering
+
+
+class Order:
+    nb_of_id = itertools.count(1)  # Create a static element incremented at the creation of the class instance
+
+    def __init__(self, quantity, price, type_of_order):
+        self._quantity = quantity
+        self._price = price
+        self._type = type_of_order
+        self._id = next(self.nb_of_id)  # Increment the static element corresponding to the id
 
     def __str__(self):  # human-readable content
-        return self._type + "%s @ %s" % (self.quantity, self.price)
+        return self._type.upper() + " %s@%s id=%s" % (self._quantity, self._price, self._id)
+
+    def __eq__(self, other):
+        # noinspection PyProtectedMember
+        return (self._quantity, self._price) == (other._quantity, other._price)
+
+    def __lt__(self, other):
+        # noinspection PyProtectedMember
+        return self._price < other._price
+
+    def get_id(self):
+        return self._id
+
+    def get_qty(self):
+        return self._quantity
+
+
+if __name__ == '__main__':
+    order1 = Order(130, 13, 'sell')
+    order2 = Order(120, 12, 'buy')
+
+    print(order1.get_id())
+    print(order2.get_id())
+    print(order2 < order1)
