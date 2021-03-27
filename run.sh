c2710ee7fdd8492b373d98352c055b3b903faa02
lesquerent <thibaultls@outlook.fr>
2021-03-27 01:31:54 +0100

    Final version of run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,10 @@
-VIRTUAL_ENV = venv
-
+#!/bin/bash
+if [ -d venv ]; then
 	source ./venv/bin/activate
-    
 else
 	python3 -m venv venv
 	source ./venv/bin/activate
+	pip install -r requirements.txt
 fi
 
-pip install -r requirements.txt
 python3 main.py
-
-
-
-
